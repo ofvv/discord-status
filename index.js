64dee74e58c6e7e7a72e9666454df590b6076b7c
Niki <contact@zirobot.xyz>
2021-05-17 14:55:41 +0300

    Now Works With Presence Update
    Now Works With presenceUpdate Event Instead Of Interval Some Code Provided By https://discord.gg/devs Also Console Logs Are Back And Improved!

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const discord = require("discord.js")
 const client = new discord.Client()
 const config = require("./config.json")
 
+/*
+some code provided by discord.gg/devs
+old code at: https://sourceb.in/duAxm5KeDr
+*/
+
 client.on("ready", async () => {
   const startlogs = [
-    `${client.user.tag} Is Starting To Scan!`,
+    `${client.user.tag} Is Starting To Check For Statuses!`,
     `Type Of Status: Custom Status`,
     `Status Content: ${config.statustext}`,
     `Role ID: ${config.roletogiveid}`
@@ -15,21 +20,25 @@ client.on("ready", async () => {
 })
 
 client.on('presenceUpdate', async (oldm, newm) => {
-    if(oldm.member.user.bot || newm.member.user.bot) return;
+  let date = new Date()
+  let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    if (oldm.member.user.bot || newm.member.user.bot) return;
 
-    let olds = oldm.activities[0].state
-    let news = newm.activities[0].state
-    if(olds === news) return;
-    if(news.includes(config.statustext) && !newm.member.roles.cache.has(config.roletogiveid)) {
-        newm.member.roles.add(config.roletogiveid).catch(() => {})
+    let olds = oldm.activities[0].state;
+    let news = newm.activities[0].state;
+    if (olds === news) return;
+    if (news.includes(config.statustext) && !newm.member.roles.cache.has(config.roletogiveid)) {
+        newm.member.roles.add(config.roletogiveid).then(async d => {
+          if (config.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE ADDED) => ${newm.user.tag} {${time}}`)
+        }).catch(() => {})
+
     } else if (!news.includes(config.statustext) && newm.member.roles.cache.has(config.roletogiveid)) {
         newm.member.roles.remove(config.roletogiveid).catch(() => {})
+        if (config.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE REMOVED) => ${newm.user.tag} {${time}}`)
     } else if (!newm.activities[0].state && newm.member.roles.cache.has(config.roletogiveid)) {
         newm.member.roles.remove(config.roletogiveid).catch(() => {})
-    } /*
-    some code provided by discord.gg/devs
-    old code at: https://sourceb.in/duAxm5KeDr
-    */
+        if (config.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE REMOVED) => ${newm.user.tag} {${time}}`)
+    }
 })
 
 
