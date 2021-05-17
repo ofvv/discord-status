92a7c92ae4740de4a3b2315dcea88922d5c21079
Niki <contact@zirobot.xyz>
2021-05-17 16:33:09 +0300

    Everything Improved!

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ old code with intervals instead of presenceUpdate event at: https://sourceb.in/d
 
 client.on("ready", async () => {
   let startdate = new Date(); let starttime = startdate.getHours() + ":" + startdate.getMinutes() + ":" + startdate.getSeconds();
-  let enabled; if (config.consolelogs === true) enabled = `Yes`; else if (config.consolelogs === false) enabled = `No`; else enabled = `Can't Detect`;
+  let enabled; if (config.options.consolelogs === true) enabled = `Yes`; else if (config.options.consolelogs === false) enabled = `No`; else enabled = `Can't Detect`;
   const startlogs = [
     `<---------------------------------------->`,
     `Bot: ${client.user.tag} (ID: ${client.user.id}) {Started: ${starttime}}`,
     `Bot Using: Discord.js v12.5.3`,
     `Github Repository: github.com/ZiroCore/discord-status`,
     `Type Of Status: Custom Status`,
-    `Status Content: ${config.statustext}`,
-    `Role ID: ${config.roletogiveid}`,
+    `Status Content: ${config.status.statustext}`,
+    `Role ID: ${config.status.roletogiveid}`,
     `Console Logs: {Enabled: ${enabled}}`,
     `<---------------------------------------->`
   ]
@@ -34,19 +34,19 @@ client.on('presenceUpdate', async (oldm, newm) => {
     let olds = oldm.activities[0].state;
     let news = newm.activities[0].state;
     if (olds === news) return;
-    if (news.includes(config.statustext) && !newm.member.roles.cache.has(config.roletogiveid)) {
-        newm.member.roles.add(config.roletogiveid).then(async d => {
-          if (config.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE ADDED) => ${newm.user.tag} {${time}}`)
+    if (news.includes(config.status.statustext) && !newm.member.roles.cache.has(config.status.roletogiveid)) {
+        newm.member.roles.add(config.status.roletogiveid).then(async d => {
+          if (config.options.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE ADDED) => ${newm.user.tag} {${time}}`)
         }).catch(() => {})
 
-    } else if (!news.includes(config.statustext) && newm.member.roles.cache.has(config.roletogiveid)) {
-        newm.member.roles.remove(config.roletogiveid).catch(() => {})
-        if (config.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE REMOVED) => ${newm.user.tag} {${time}}`)
-    } else if (!newm.activities[0].state && newm.member.roles.cache.has(config.roletogiveid)) {
-        newm.member.roles.remove(config.roletogiveid).catch(() => {})
-        if (config.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE REMOVED) => ${newm.user.tag} {${time}}`)
+    } else if (!news.includes(config.status.statustext) && newm.member.roles.cache.has(config.status.roletogiveid)) {
+        newm.member.roles.remove(config.status.roletogiveid).catch(() => {})
+        if (config.options.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE REMOVED) => ${newm.user.tag} {${time}}`)
+    } else if (!newm.activities[0].state && newm.member.roles.cache.has(config.status.roletogiveid)) {
+        newm.member.roles.remove(config.status.roletogiveid).catch(() => {})
+        if (config.options.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE REMOVED) => ${newm.user.tag} {${time}}`)
     }
 })
 
 
-client.login(config.token)
+client.login(config.bot.token)
