e5ca2c65e4a890aeb2fcd7fbde0e9c37fa0febf5
Niki <contact@zirobot.xyz>
2021-05-18 13:28:30 +0300

    added anystatus

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ client.on('presenceUpdate', async (oldm, newm) => {
     let olds = oldm.activities[0].state;
     let news = newm.activities[0].state;
     if (olds === news) return;
+    if (config.status.anystatus === false) {
     if (news.includes(config.status.statustext) && !newm.member.roles.cache.has(config.status.roletogiveid)) {
         newm.member.roles.add(config.status.roletogiveid).then(async d => {
           if (config.options.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE ADDED) => ${newm.user.tag} {${time}}`)
@@ -47,6 +48,13 @@ client.on('presenceUpdate', async (oldm, newm) => {
         newm.member.roles.remove(config.status.roletogiveid).catch(() => {})
         if (config.options.consolelogs === true) console.log(`[CONSOLE LOG] (ROLE REMOVED) => ${newm.user.tag} {${time}}`)
     }
+  } else if (config.status.anystatus === true) {
+    if (news && olds) {
+      newm.member.roles.add(config.status.roletogiveid).then(async d => {
+        if (config.options.consolelogs === true) console.log(`[CONSOLE LOG] ANY STATUS (ROLE ADDED) => ${newm.user.tag} {${time}}`)
+      }).catch(() => {})
+    }
+  }
   } catch (e) {}
 })
 
